98c44e1acd0e72092182af4081a809957bc43e0c
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-11-26 10:38:25 +0500

    chore: remove real entities and fix entity base

--- Core/Entities/Entity.cs
+++ Core/Entities/Entity.cs
@@ -1,17 +1,13 @@
 ﻿namespace Core.Entities;
 
-public class WorkEntry : EntityBase
+public class Entity : EntityBase
 {
     // EntityFrameworkCore related empty default constructor
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-    public WorkEntry()
+    public Entity()
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     {
     }
 
-    public long ProjectId { get; set; }
-
-    public string Title { get; set; }
-    
-    public string TaskId { get; set; }
+    public long EntityProp { get; set; }
 }
--- Core/EntityBase.cs
+++ Core/EntityBase.cs
@@ -1,26 +1,8 @@
-﻿using Microsoft.VisualBasic;
-
-namespace Core;
+﻿namespace Core;
 
 public abstract class EntityBase
 {
     public long Id { get; set; }
 
     public long TenantId { get; set; }
-
-    public long EmployeeId { get; set; }
-
-    public DateTime StartTime { get; set; }
-
-    public DateTime EndTime { get; set; }
-
-    public string TimeZoneId { get; set; }
-
-    public DateInterval Duration { get; set; }
-
-    public int Type { get; set; }
-
-    public string Description { get; set; }
-
-    public bool IsDeleted { get; set; }
 }
