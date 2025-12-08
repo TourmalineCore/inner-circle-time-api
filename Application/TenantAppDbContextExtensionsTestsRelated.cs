04baec100d5eeaad9b3cce02f3bf42fb063e9021
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2025-12-08 14:02:49 +0500

    fix: remove seed in random function to generate unique dbName to prevent conflict in tests

--- Application/TenantAppDbContextExtensionsTestsRelated.cs
+++ Application/TenantAppDbContextExtensionsTestsRelated.cs
@@ -13,7 +13,7 @@ internal static class TenantAppDbContextExtensionsTestsRelated
             .UseInMemoryDatabase(
                 // we need a unique db name so that tests of the same collection can run in isolation
                 // otherwise they inrefere and see each others data
-                new Random(100500).Next().ToString(),
+                new Random().Next().ToString(),
                 // we want to provide as little setup data as possible to check a certain piece of a flow
                 // thus, we don't want to specify all properties of seeded data when it isn't used by the logic
                 // for instance, I need to check that an entity exists by Id, I don't need to setup its required Name property
