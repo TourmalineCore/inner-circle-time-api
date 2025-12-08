ed6100825eb76cb29ac1c88d5658be3d1891d2c9
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-12-08 10:59:32 +0500

    refactor: remove unnecessary null initialization

--- Core/Entities/WorkEntry.cs
+++ Core/Entities/WorkEntry.cs
@@ -26,10 +26,10 @@ public class WorkEntry : EntityBase
 
     public string Title { get; set; }
 
-    public string? TaskId { get; set; } = null;
+    public string? TaskId { get; set; }
 
     // TODO: make it required when we add this prop to frontend
-    public string? Description { get; set; } = null;
+    public string? Description { get; set; }
 
-    public bool? IsDeleted { get; set; } = false;
+    public bool IsDeleted { get; set; } = false;
 }
