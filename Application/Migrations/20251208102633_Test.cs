e741cb5d06d094093df9a3d7da21108e61b05e61
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-12-09 15:16:05 +0500

    feat: add migation

---
+++ Application/Migrations/20251208102633_Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "work_entries",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    employee_id = table.Column<long>(type: "bigint", nullable: false),
+                    start_time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    end_time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    time_zone_id = table.Column<string>(type: "text", nullable: true),
+                    duration = table.Column<TimeSpan>(type: "interval", nullable: false, computedColumnSql: "end_time - start_time", stored: true),
+                    type = table.Column<int>(type: "integer", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    task_id = table.Column<string>(type: "text", nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    tenant_id = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_work_entries", x => x.id);
+                    table.CheckConstraint("ck_work_entries_type_not_zero", "\"type\" <> 0");
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "work_entries");
+        }
+    }
+}
