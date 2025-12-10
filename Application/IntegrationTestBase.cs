1ae52c29816d9fa4a16d7f32d6e2da05f5a26c6d
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-12-10 15:56:55 +0500

    refactor: remove unnecassary ApplyMigrationsAsync

--- Application/IntegrationTestBase.cs
+++ Application/IntegrationTestBase.cs
@@ -30,8 +30,6 @@ public class IntegrationTestBase : IAsyncLifetime
 
         await _dbConnection.OpenAsync();
 
-        await ApplyMigrationsAsync();
-
         // Begin Transaction
         _dbTransaction = await _dbConnection.BeginTransactionAsync();
 
@@ -54,20 +52,6 @@ public class IntegrationTestBase : IAsyncLifetime
 
         if (_dbConnection != null)
             await _dbConnection.CloseAsync();
-    }
-
-    private async Task ApplyMigrationsAsync()
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseNpgsql(_dbConnection);
-
-        using var context = new AppDbContext(optionsBuilder.Options);
-
-        var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
-        if (pendingMigrations.Any())
-        {
-            await context.Database.MigrateAsync();
-        }
     }
 
     protected TenantAppDbContext CreateTenantDbContext()
