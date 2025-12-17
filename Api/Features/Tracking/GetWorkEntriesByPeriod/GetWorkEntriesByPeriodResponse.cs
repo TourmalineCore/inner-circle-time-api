5c07fb62e1b921182199b9400d823ec711b339c8
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-12-17 14:16:20 +0500

    fix: add the missing required modification

--- Api/Features/Tracking/GetWorkEntriesByPeriod/GetWorkEntriesByPeriodResponse.cs
+++ Api/Features/Tracking/GetWorkEntriesByPeriod/GetWorkEntriesByPeriodResponse.cs
@@ -7,13 +7,13 @@ public class GetWorkEntriesByPeriodResponse
 
 public class WorkEntryItem
 {
-    public long Id { get; set; }
+    public required long Id { get; set; }
 
     public required string Title { get; set; }
 
-    public DateTime StartTime { get; set; }
+    public required DateTime StartTime { get; set; }
 
-    public DateTime EndTime { get; set; }
+    public required DateTime EndTime { get; set; }
 
     public required string TaskId { get; set; }
 
