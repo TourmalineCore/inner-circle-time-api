fb14592f18c6f8e5f511884758e50c9a84aa4160
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-12-17 13:52:29 +0500

    chore: remove empty line

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using TourmalineCore.AspNetCore.JwtAuthentication.Core;
 using TourmalineCore.AspNetCore.JwtAuthentication.Core.Options;
 
-
 namespace Api;
 
 public class Program
