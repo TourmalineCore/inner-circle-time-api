8b1e8a7dbf85f640717ba4bc76520c26772be154
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-12-18 09:29:31 +0500

    feat: #8: add HttpClientTest and CreateWorkEntryAsync_ShouldThrowValidationErrorIfAtLeastOneOfRequiredFieldsIsEmpty test

---
+++ Api/Features/Tracking/TrackingControllerTests.cs
@@ -0,0 +1,41 @@
+using Application.Commands;
+using Application.TestsConfig;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Api.Features.Tracking;
+
+[IntegrationTest]
+public class TrackingControllerValidationTests : HttpClientTest
+{
+    public TrackingControllerValidationTests(WebApplicationFactory<Program> factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task CreateWorkEntryAsync_ShouldThrowValidationErrorIfAtLeastOneOfRequiredFieldsIsEmpty()
+    {
+        var createWorkEntryCommandParams = new CreateWorkEntryCommandParams
+        {
+            Title = "",
+            StartTime = new DateTime(2025, 11, 24, 9, 0, 0),
+            EndTime = new DateTime(2025, 11, 24, 10, 0, 0),
+            TaskId = "#2231",
+            Description = "Task description",
+            Type = EventType.Task
+        };
+
+        var response = await HttpClient.PostAsJsonAsync("/api/time/tracking/work-entries", createWorkEntryCommandParams);
+
+        Assert.NotNull(response);
+        Assert.Equal(StatusCodes.Status400BadRequest, (int)response.StatusCode);
+
+        var validationProblemDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+        Assert.NotNull(validationProblemDetails);
+        Assert.Equal("Fill in all the fields", validationProblemDetails.Detail);
+        Assert.Equal("Validation error", validationProblemDetails.Title);
+    }
+}
