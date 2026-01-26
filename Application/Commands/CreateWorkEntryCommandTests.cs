4c52ff512374a45f5063d26c00dc0f4ff86b0bef
Ekaterina <echekina@gmail.com>
2026-01-26 15:26:29 +0500

    refactor: #7: delete redundant tests on Create & UpdateWorkEntryCommand already covered in karate

--- Application/Commands/CreateWorkEntryCommandTests.cs
+++ Application/Commands/CreateWorkEntryCommandTests.cs
@@ -9,45 +9,6 @@ namespace Application.Commands;
 public class CreateWorkEntryCommandTests : IntegrationTestBase
 {
     [Fact]
-    public async Task CreateWorkEntryAsync_ShouldSaveNewWorkEntryToDb()
-    {
-        var context = CreateTenantDbContext();
-
-        var mockClaimsProvider = GetMockClaimsProvider();
-
-        var mockAssignmentsApi = GetMockAssignmentsApi();
-
-        var createWorkEntryCommand = new CreateWorkEntryCommand(context, mockClaimsProvider, mockAssignmentsApi);
-
-        var createWorkEntryCommandParams = new CreateWorkEntryCommandParams
-        {
-            Title = "Task 1",
-            StartTime = new DateTime(2025, 11, 24, 9, 0, 0),
-            EndTime = new DateTime(2025, 11, 24, 10, 0, 0),
-            ProjectId = 1,
-            TaskId = "#2231",
-            Description = "Task description",
-            Type = EventType.Task
-        };
-
-        var newWorkEntryId = await createWorkEntryCommand.ExecuteAsync(createWorkEntryCommandParams);
-
-        var newWorkEntry = await FindEntityAsync<WorkEntry>(context, newWorkEntryId);
-
-        Assert.NotNull(newWorkEntry);
-        Assert.Equal(createWorkEntryCommandParams.Title, newWorkEntry.Title);
-        Assert.Equal(EMPLOYEE_ID, newWorkEntry.EmployeeId);
-        Assert.Equal(TENANT_ID, newWorkEntry.TenantId);
-        Assert.Equal(createWorkEntryCommandParams.ProjectId, newWorkEntry.ProjectId);
-        Assert.Equal(createWorkEntryCommandParams.TaskId, newWorkEntry.TaskId);
-        Assert.Equal(createWorkEntryCommandParams.StartTime, newWorkEntry.StartTime);
-        Assert.Equal(createWorkEntryCommandParams.EndTime, newWorkEntry.EndTime);
-        Assert.Equal(createWorkEntryCommandParams.Type, newWorkEntry.Type);
-        Assert.Equal(createWorkEntryCommandParams.Description, newWorkEntry.Description);
-        Assert.Equal(createWorkEntryCommandParams.EndTime - createWorkEntryCommandParams.StartTime, newWorkEntry.Duration);
-    }
-
-    [Fact]
     public async Task CreateWorkEntryAsync_ShouldThrowErrorIfTypeIsUnspecified()
     {
         var context = CreateTenantDbContext();
