ea9b57668baa2b7ebd0fcb5289595a85315cc0d3
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-01-27 16:30:52 +0500

    fix: #12: return CreateWorkEntryAsync_ShouldThrowConflictingTimeRangeExceptionIfTimeConflictsWithAnotherTask test

--- Application/Commands/CreateWorkEntryCommandTests.cs
+++ Application/Commands/CreateWorkEntryCommandTests.cs
@@ -95,4 +95,42 @@ public class CreateWorkEntryCommandTests : IntegrationTestBase
         Assert.Contains("ck_work_entries_end_time_is_greater_than_start_time", exception.InnerException!.InnerException!.Message);
         Assert.Equal("End time must be greater than start time", exception.Message);
     }
+
+    [Fact]
+    public async Task CreateWorkEntryAsync_ShouldThrowConflictingTimeRangeExceptionIfTimeConflictsWithAnotherTask()
+    {
+        var context = CreateTenantDbContext();
+
+        var mockClaimsProvider = GetMockClaimsProvider();
+
+        var workEntry = await SaveEntityAsync(context, new WorkEntry
+        {
+            EmployeeId = EMPLOYEE_ID,
+            Title = "Task 1",
+            StartTime = new DateTime(2025, 11, 24, 9, 0, 0),
+            EndTime = new DateTime(2025, 11, 24, 10, 0, 0),
+            TaskId = "#2231",
+            Description = "Task description",
+            Type = EventType.Task
+        });
+
+        var createWorkEntryCommand = new CreateWorkEntryCommand(context, mockClaimsProvider);
+
+        var createWorkEntryCommandParams = new CreateWorkEntryCommandParams
+        {
+            Title = "Task 2",
+            StartTime = new DateTime(2025, 11, 24, 9, 0, 0),
+            EndTime = new DateTime(2025, 11, 24, 11, 0, 0),
+            TaskId = "#2232",
+            Description = "Task description",
+            Type = EventType.Task
+        };
+
+        var exception = await Assert.ThrowsAsync<ConflictingTimeRangeException>(
+            async () => await createWorkEntryCommand.ExecuteAsync(createWorkEntryCommandParams)
+        );
+
+        Assert.Contains("ck_work_entries_no_time_overlap", exception.InnerException!.InnerException!.Message);
+        Assert.Equal("Another task is scheduled for this time", exception.Message);
+    }
 }
