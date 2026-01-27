2b5673d8b094b0effcc589b7b8b9f3ea0ce31417
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2026-01-27 13:46:18 +0500

    refactor: delete redundant Exception constructors

--- Application/Exceptions/ConflictingTimeRangeException.cs
+++ Application/Exceptions/ConflictingTimeRangeException.cs
@@ -1,6 +1,4 @@
 public class ConflictingTimeRangeException : Exception
 {
-    public ConflictingTimeRangeException() { }
-    public ConflictingTimeRangeException(string message) : base(message) { }
     public ConflictingTimeRangeException(string message, Exception inner) : base(message, inner) { }
 }
