43f630fe046a50854671b38ca2bf4c75c03110a6
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2026-01-27 11:53:00 +0500

    refactor: delete redundant Exception constructors

--- Application/Exceptions/InvalidTimeRangeException.cs
+++ Application/Exceptions/InvalidTimeRangeException.cs
@@ -1,6 +1,4 @@
 public class InvalidTimeRangeException : Exception
 {
-    public InvalidTimeRangeException() { }
-    public InvalidTimeRangeException(string message) : base(message) { }
     public InvalidTimeRangeException(string message, Exception inner) : base(message, inner) { }
 }
