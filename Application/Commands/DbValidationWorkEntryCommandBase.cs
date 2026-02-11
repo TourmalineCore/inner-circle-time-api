967f24b052371fb01b8508c122e34e7ad5a32b0c
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-02-11 09:28:16 +0500

    refactor: #12: create constants for the constraint name and add comments about the need to double check the exception

--- Application/Commands/DbValidationWorkEntryCommandBase.cs
+++ Application/Commands/DbValidationWorkEntryCommandBase.cs
@@ -5,15 +5,20 @@ using Npgsql;
 public abstract class DbValidationWorkEntryCommandBase<TCommandParams>
     where TCommandParams : class
 {
+    private const string CK_WORK_ENTRIES_END_TIME_IS_GREATER_THAN_START_TIME = "ck_work_entries_end_time_is_greater_than_start_time";
+    private const string CK_WORK_ENTRIES_NO_TIME_OVERLAP = "ck_work_entries_no_time_overlap";
+
     public async Task<long> MakeChangesInDbAsync(TCommandParams commandParams)
     {
         try
         {
             return await MakeChangesToWorkEntryAsync(commandParams);
         }
+        // Double checking is necessary because different operations (SaveChangesAsync, ExecuteUpdateAsync) generate different exceptions
+        // Depending on the type of exception, the ConstraintName can be both in the exception and in the internal exception 
         catch (Exception e) when (
-            (e.InnerException as PostgresException)?.ConstraintName == "ck_work_entries_end_time_is_greater_than_start_time" ||
-            (e as PostgresException)?.ConstraintName == "ck_work_entries_end_time_is_greater_than_start_time"
+            (e.InnerException as PostgresException)?.ConstraintName == CK_WORK_ENTRIES_END_TIME_IS_GREATER_THAN_START_TIME ||
+            (e as PostgresException)?.ConstraintName == CK_WORK_ENTRIES_END_TIME_IS_GREATER_THAN_START_TIME
         )
         {
             throw new InvalidTimeRangeException(
@@ -22,8 +27,8 @@ public abstract class DbValidationWorkEntryCommandBase<TCommandParams>
             );
         }
         catch (Exception e) when (
-            (e.InnerException as PostgresException)?.ConstraintName == "ck_work_entries_no_time_overlap" ||
-            (e as PostgresException)?.ConstraintName == "ck_work_entries_no_time_overlap"
+            (e.InnerException as PostgresException)?.ConstraintName == CK_WORK_ENTRIES_NO_TIME_OVERLAP ||
+            (e as PostgresException)?.ConstraintName == CK_WORK_ENTRIES_NO_TIME_OVERLAP
         )
         {
             throw new ConflictingTimeRangeException(
