4fafe4a2e959cf8fdf86b4bf2237eea0f3f5bf69
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-02-11 09:01:35 +0500

    refcator: #12: add error example which occurs in integration tests

--- Application/Commands/WorkEntryCommandTestsBaseTestsRelated.cs
+++ Application/Commands/WorkEntryCommandTestsBaseTestsRelated.cs
@@ -1,11 +1,14 @@
-﻿
-// It partial class used in create and update tests in order to run these tests sequentially,
-// this avoids a race condition between these tests
-// https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/partial-classes-and-methods
-using Application.TestsConfig;
+﻿using Application.TestsConfig;
 
 namespace Application.Commands;
 
+
+// It partial class used in create and update tests in order to run these tests sequentially,
+// this avoids a race condition between these tests
+// https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/partial-classes-and-methods
+// Error example: System.InvalidOperationException : An exception has been raised that is likely due to a transient failure.
+//---- Microsoft.EntityFrameworkCore.DbUpdateException : An error occurred while saving the entity changes. See the inner exception for details.
+//-------- Npgsql.PostgresException : 40P01: deadlock detected
 [IntegrationTest]
 public partial class WorkEntryCommandTestsBase : IntegrationTestBase
 {
