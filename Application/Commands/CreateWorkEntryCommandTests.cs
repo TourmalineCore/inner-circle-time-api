0fc5efec8c192eb6cea23d50bd63aea4c337eea0
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-02-18 08:35:51 +0500

    refactor: #27: remove test which checks ck_work_entries_type_not_zero constraint

--- Application/Commands/CreateWorkEntryCommandTests.cs
+++ Application/Commands/CreateWorkEntryCommandTests.cs
@@ -5,33 +5,6 @@ namespace Application.Commands;
 
 public partial class WorkEntryCommandTestsBase
 {
-    // [Fact]
-    // public async Task CreateWorkEntryAsync_ShouldThrowErrorIfTypeIsUnspecified()
-    // {
-    //     var context = CreateTenantDbContext();
-
-    //     var mockClaimsProvider = GetMockClaimsProvider();
-
-    //     var createWorkEntryCommand = new CreateWorkEntryCommand(context, mockClaimsProvider);
-
-    //     var createWorkEntryCommandParams = new CreateWorkEntryCommandParams
-    //     {
-    //         Title = "Task 1",
-    //         StartTime = new DateTime(2025, 11, 24, 9, 0, 0),
-    //         EndTime = new DateTime(2025, 11, 24, 10, 0, 0),
-    //         ProjectId = 1,
-    //         TaskId = "#2231",
-    //         Description = "Task description",
-    //         Type = EventType.Unspecified
-    //     };
-
-    //     var exception = await Assert.ThrowsAsync<DbUpdateException>(
-    //         async () => await createWorkEntryCommand.ExecuteAsync(createWorkEntryCommandParams)
-    //     );
-
-    //     Assert.Contains("ck_work_entries_type_not_zero", exception.InnerException!.Message);
-    // }
-
     [Fact]
     public async Task CreateWorkEntryAsync_ShouldThrowInvalidTimeRangeExceptionIfStartTimeIsGreaterEndTime()
     {
