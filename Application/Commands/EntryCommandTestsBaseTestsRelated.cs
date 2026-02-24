e07de857d781cc8e8f245f136423c892d0d2fb95
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-02-24 15:57:10 +0500

    Apply suggestion from @SaraevaAnastasia
    Co-authored-by: SaraEva <74062874+SaraevaAnastasia@users.noreply.github.com>

--- Application/Commands/EntryCommandTestsBaseTestsRelated.cs
+++ Application/Commands/EntryCommandTestsBaseTestsRelated.cs
@@ -5,7 +5,7 @@ namespace Application.Commands;
 // This partial class was introduced to inherited by create and update commands tests in order to run these tests sequentially
 // https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/partial-classes-and-methods
 
-// Before that there were 2 separate clasees and they were running cincurrently and often lead to a deadlock and thus tests were flacky
+// Before that there were 2 separate classes and they were running concurrently and often lead to a deadlock and thus tests were flaky
 // There is an issue to investigate the root cause why they fail https://github.com/TourmalineCore/inner-circle-time-api/issues/26
 [IntegrationTest]
 public partial class EntryCommandTestsBase : IntegrationTestBase
