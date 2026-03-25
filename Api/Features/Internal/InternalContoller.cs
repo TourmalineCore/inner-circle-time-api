96a55f2ca31a26cd40f603aebadf7d1dd2f3cfac
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2026-03-25 14:46:09 +0500

    feat: #64: add permissions and authorize to internal controller

--- Api/Features/Internal/InternalContoller.cs
+++ Api/Features/Internal/InternalContoller.cs
@@ -1,15 +1,19 @@
 using System.ComponentModel.DataAnnotations;
 using Application.ExternalDeps.AssignmentsApi;
 using Application.Features.Internal.GetEmployeesTrackedTaskHoursByProject;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TourmalineCore.AspNetCore.JwtAuthentication.Core.Filters;
 
 namespace Api.Features.Internal;
 
+[Authorize]
 [ApiController]
-[Route("internal")]
+[Route("/api/internal")]
 public class InternalController : ControllerBase
 {
     [EndpointSummary("Get employees time entries by project")]
+    [RequiresPermission(UserClaimsProvider.CanViewAllTimeTrackerEntries)]
     [HttpGet("projects/tracked-task-hours")]
     public Task<GetEmployeesTrackedTaskHoursByProjectResponse> GetEmployeesEntriesByProjectAndPeriod(
         [Required][FromQuery] long projectId,
@@ -22,6 +26,7 @@ public class InternalController : ControllerBase
     }
 
     [EndpointSummary("Get all projects")]
+    [RequiresPermission(UserClaimsProvider.CanViewAllProjects)]
     [HttpGet("projects")]
     public async Task<ProjectsResponse> GetAllProjectsAsync(
         [FromServices] IAssignmentsApi assignmentsApi
