a974146d4057b1f3939fdba2863a3dc3c1dc9de0
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-04-02 15:22:27 +0500

    fix: #70: change double to decimal for tracked hours for more accurate counting tracked hours

--- Core/TotalTrackedTaskHoursCalculatorTests.cs
+++ Core/TotalTrackedTaskHoursCalculatorTests.cs
@@ -33,7 +33,7 @@ public class TotalTrackedTaskHoursCalculatorTests
         var firstEmployee = calculatedEmployeesTaskEntries.Single(x => x.EmployeeId == 1);
 
         Assert.Equal(1, firstEmployee.EmployeeId);
-        Assert.Equal(2.9833333333333334, firstEmployee.TrackedHours);
+        Assert.Equal((decimal)2.9833333333333334, firstEmployee.TrackedHours);
     }
 
     [Fact]
@@ -85,9 +85,44 @@ public class TotalTrackedTaskHoursCalculatorTests
         var secondEmployee = calculatedEmployeesTaskEntries.Single(x => x.EmployeeId == 2);
 
         Assert.Equal(1, firstEmployee.EmployeeId);
-        Assert.Equal(2.75, firstEmployee.TrackedHours);
+        Assert.Equal((decimal)2.75, firstEmployee.TrackedHours);
 
         Assert.Equal(2, secondEmployee.EmployeeId);
-        Assert.Equal(4.5, secondEmployee.TrackedHours);
+        Assert.Equal((decimal)4.5, secondEmployee.TrackedHours);
+    }
+
+    [Fact]
+    public void TotalTrackedTaskHoursCalculatorWithIncompleteHours_ShouldReturnCorrectEmployeesTrackedTaskHours()
+    {
+        var employeesTaskEntries = new List<TaskEntry> {
+            new TaskEntry
+            {
+                Id = 1,
+                EmployeeId = 1,
+                Duration = TimeSpan.Parse("08:00:00")
+            },
+            new TaskEntry {
+                Id = 2,
+                EmployeeId = 1,
+                Duration = TimeSpan.Parse("00:20:00")
+            },
+            new TaskEntry {
+                Id = 3,
+                EmployeeId = 1,
+                Duration = TimeSpan.Parse("00:20:00")
+            },
+            new TaskEntry {
+                Id = 4,
+                EmployeeId = 1,
+                Duration = TimeSpan.Parse("00:20:00")
+            },
+        };
+
+        var calculatedEmployeesTaskEntries = TotalTrackedTaskHoursCalculator.Calculate(employeesTaskEntries);
+
+        var firstEmployee = calculatedEmployeesTaskEntries.Single(x => x.EmployeeId == 1);
+
+        Assert.Equal(1, firstEmployee.EmployeeId);
+        Assert.Equal(9, firstEmployee.TrackedHours);
     }
 }
