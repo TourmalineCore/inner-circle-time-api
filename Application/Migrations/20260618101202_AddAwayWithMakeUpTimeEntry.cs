b726a7c5355a62f3274ad00a3413041193818349
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-06-18 16:19:14 +0500

    feat: #96: add AddAwayWithMakeUpTimeEntry migration

---
+++ Application/Migrations/20260618101202_AddAwayWithMakeUpTimeEntry.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAwayWithMakeUpTimeEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "discriminator",
+                table: "tracked_entries",
+                type: "character varying(34)",
+                maxLength: 34,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AddColumn<long>(
+                name: "related_entry_id",
+                table: "tracked_entries",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_tracked_entries_related_entry_id",
+                table: "tracked_entries",
+                column: "related_entry_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_tracked_entries_tracked_entries_related_entry_id",
+                table: "tracked_entries",
+                column: "related_entry_id",
+                principalTable: "tracked_entries",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_tracked_entries_tracked_entries_related_entry_id",
+                table: "tracked_entries");
+
+            migrationBuilder.DropIndex(
+                name: "ix_tracked_entries_related_entry_id",
+                table: "tracked_entries");
+
+            migrationBuilder.DropColumn(
+                name: "related_entry_id",
+                table: "tracked_entries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "discriminator",
+                table: "tracked_entries",
+                type: "character varying(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(34)",
+                oldMaxLength: 34);
+        }
+    }
+}
