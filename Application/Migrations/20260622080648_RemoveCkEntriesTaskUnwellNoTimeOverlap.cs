7ae70b5d3270d19dfbb97ab6f41f0f8fd720ed7a
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-06-22 16:48:48 +0500

    feat: #96: add RemoveCkEntriesTaskUnwellNoTimeOverlap and  AddCkEntriesNoTimeOverlapConstraint migrations

---
+++ Application/Migrations/20260622080648_RemoveCkEntriesTaskUnwellNoTimeOverlap.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCkEntriesTaskUnwellNoTimeOverlap : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Drop constraint
+            migrationBuilder.Sql(@"
+            ALTER TABLE tracked_entries 
+            DROP CONSTRAINT IF EXISTS ck_entries_task_unwell_no_time_overlap;
+        ");
+            // Drop btree_gist extension
+            migrationBuilder.Sql(@"
+            DROP EXTENSION IF EXISTS btree_gist;
+        ");
+        }
+
+
+        // Reverts the migration and removes the constraint and exstention if it exists.
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE EXTENSION IF NOT EXISTS btree_gist;
+            ");
+
+            migrationBuilder.Sql(@"
+                ALTER TABLE tracked_entries
+                ADD CONSTRAINT ck_entries_task_unwell_no_time_overlap
+                EXCLUDE USING GIST (
+                    tenant_id WITH =,
+                    employee_id WITH =,
+                    tsrange(start_time, end_time, '[)') WITH &&
+                )
+                WHERE (type IN (1, 2) AND deleted_at_utc IS NULL); 
+            ");
+        }
+    }
+}
