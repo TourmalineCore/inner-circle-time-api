ec76c558db0b63a3864c30145819095a29c5b3d2
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-06-23 10:09:18 +0500

    cleanup: #96: remove duplicate test

--- Application/Features/Tracking/UpdateTaskEntry/UpdateTaskEntryCommandTests.cs
+++ Application/Features/Tracking/UpdateTaskEntry/UpdateTaskEntryCommandTests.cs
@@ -52,56 +52,6 @@ public class UpdateTaskEntryCommandTests : IntegrationTestBase
     }
 
     [Fact]
-    public async Task UpdateTaskEntryAsync_ShouldThrowConflictingTimeRangeExceptionIfTimeConflictsWithAnotherTask()
-    {
-        var context = CreateTenantDbContext();
-
-        var mockClaimsProvider = MockClaimsProviderFactory.CreateMock(EMPLOYEE_ID, TENANT_ID);
-
-        var taskEntry = await SaveEntityAsync(context, new TaskEntry
-        {
-            EmployeeId = EMPLOYEE_ID,
-            Title = "Task 1",
-            StartTime = new DateTime(2025, 11, 24, 9, 0, 0),
-            EndTime = new DateTime(2025, 11, 24, 10, 0, 0),
-            TaskId = "#2231",
-            ProjectId = 1,
-            Description = "Task description",
-        });
-
-        var taskEntry2 = await SaveEntityAsync(context, new TaskEntry
-        {
-            EmployeeId = EMPLOYEE_ID,
-            Title = "Task 2",
-            StartTime = new DateTime(2025, 11, 24, 11, 0, 0),
-            EndTime = new DateTime(2025, 11, 24, 12, 0, 0),
-            TaskId = "#2231",
-            ProjectId = 1,
-            Description = "Task description",
-        });
-
-        var updateTaskEntryCommand = new UpdateTaskEntryCommand(context, mockClaimsProvider);
-
-        var updateTaskEntryRequest = new UpdateTaskEntryRequest
-        {
-            Id = taskEntry2.Id,
-            Title = "Task 2",
-            StartTime = new DateTime(2025, 11, 24, 9, 0, 0),
-            EndTime = new DateTime(2025, 11, 24, 11, 0, 0),
-            TaskId = "#2232",
-            ProjectId = 1,
-            Description = "Task description",
-        };
-
-        var exception = await Assert.ThrowsAsync<ConflictingTimeRangeException>(
-            async () => await updateTaskEntryCommand.ExecuteAsync(updateTaskEntryRequest)
-        );
-
-        Assert.Contains("ck_entries_task_unwell_no_time_overlap", exception.InnerException!.Message);
-        Assert.Equal("Another task is scheduled for this time", exception.Message);
-    }
-
-    [Fact]
     public async Task UpdateTaskEntryAsync_ShouldNotUpdateDeletedTaskEntry()
     {
         var context = CreateTenantDbContext();
