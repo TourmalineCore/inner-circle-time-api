68d5cff73592b3df28fdfe6d6ba68d002d657955
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-07-08 15:36:37 +0500

    refactor: #115: add JsonIgnore attribute to UpdateSickLeaveEntryRequest to hide this property from swagger

--- Application/Features/Tracking/UpdateSickLeaveEntry/UpdateSickLeaveEntryRequest.cs
+++ Application/Features/Tracking/UpdateSickLeaveEntry/UpdateSickLeaveEntryRequest.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Application.Features.Tracking.UpdateSickLeaveEntry;
 
 public class UpdateSickLeaveEntryRequest
 {
+    [JsonIgnore]
     public long Id { get; set; }
 
     [Required]
