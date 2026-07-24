0b161dabec3c3b63c82b006d857857ffadb606f4
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2026-07-24 14:53:47 +0500

    refactor: #109: remove duplication in GetEntriesByPeriodQueryTests using MemberData

--- Application/Features/Tracking/GetEntriesByPeriod/GetEntriesByPeriodQueryTests.cs
+++ Application/Features/Tracking/GetEntriesByPeriod/GetEntriesByPeriodQueryTests.cs
@@ -119,36 +119,72 @@ public class GetEntriesByPeriodQueryTests
         Assert.DoesNotContain(result, x => x.Id == taskEntry.Id);
     }
 
-    [Fact]
-    public async Task GetEntriesByPeriodAsync_ShouldReturnEntryWhenEntryEndsExactlyOnPeriodStart()
+
+    public static IEnumerable<object[]> SickLeaveEntryOverlapsPeriodTestData()
+
     {
-        var context = TenantAppDbContextExtensionsTestsRelated.CreateInMemoryTenantContextForTests(TENANT_ID);
-
-        var sickLeaveEntry = new SickLeaveEntry
+        return new List<object[]>
         {
-            Id = 1,
-            EmployeeId = EMPLOYEE_ID,
-            TenantId = TENANT_ID,
-            StartTime = new DateTime(2026, 7, 13, 0, 0, 0),
-            EndTime = new DateTime(2026, 7, 20, 0, 0, 0),
-            Type = EntryType.SickLeave
+            // sick leave starts before the period and ends on the first day of the period
+            new object[] {
+                new DateTime(2026, 7, 13, 0, 0, 0), // sickLeaveStartTime
+                new DateTime(2026, 7, 20, 0, 0, 0), // sickLeaveEndTime
+                new DateOnly(2026, 7, 20),          // periodStartDate
+                new DateOnly(2026, 7, 26),          // periodEndDate
+                true                                // shouldEntryBeReturned
+            },
+            // sick leave starts on the last day of the period and lasts longer than the period
+            new object[] {
+                new DateTime(2026, 7, 13, 0, 0, 0),
+                new DateTime(2026, 7, 20, 0, 0, 0),
+                new DateOnly(2026, 7, 10),
+                new DateOnly(2026, 7, 13),
+                true
+            },
+            // sick leave starts and ends before the period
+            new object[] {
+                new DateTime(2026, 7, 10, 0, 0, 0),
+                new DateTime(2026, 7, 12, 0, 0, 0),
+                new DateOnly(2026, 7, 13),
+                new DateOnly(2026, 7, 19),
+                false
+            },
+            // sick leave starts and ends after the period
+            new object[] {
+                new DateTime(2026, 7, 20, 0, 0, 0),
+                new DateTime(2026, 7, 26, 0, 0, 0),
+                new DateOnly(2026, 7, 13),
+                new DateOnly(2026, 7, 19),
+                false
+            },
+            // sick leave starts before the period and ends after the period
+            new object[] {
+                new DateTime(2026, 7, 20, 0, 0, 0),
+                new DateTime(2026, 7, 26, 0, 0, 0),
+                new DateOnly(2026, 7, 21),
+                new DateOnly(2026, 7, 25),
+                true
+            },
+            // sick leave is within the period
+            new object[] {
+                new DateTime(2026, 7, 21, 0, 0, 0),
+                new DateTime(2026, 7, 24, 0, 0, 0),
+                new DateOnly(2026, 7, 20),
+                new DateOnly(2026, 7, 25),
+                true
+            }
         };
-
-        await context.AddEntityAndSaveAsync(sickLeaveEntry);
-
-        var getEntriesByPeriodQuery = new GetEntriesByPeriodQuery(context, _mockClaimsProvider);
-
-        var startDate = new DateOnly(2026, 7, 20);
-        var endDate = new DateOnly(2026, 7, 26);
-
-        var result = await getEntriesByPeriodQuery.GetByPeriodAsync<TrackedEntryBase>(startDate, endDate);
-
-        Assert.NotEmpty(result);
-        Assert.Contains(result, x => x.Id == sickLeaveEntry.Id);
     }
 
-    [Fact]
-    public async Task GetEntriesByPeriodAsync_ShouldReturnEntryWhenEntryStartsExactlyOnPeriodEnd()
+    [Theory]
+    [MemberData(nameof(SickLeaveEntryOverlapsPeriodTestData))]
+    public async Task GetEntriesByPeriodAsync_ShouldReturnEntryWhenItOverlapsPeriod(
+        DateTime sickLeaveStartTime,
+        DateTime sickLeaveEndTime,
+        DateOnly periodStartDate,
+        DateOnly periodEndDate,
+        bool shouldEntryBeReturned
+    )
     {
         var context = TenantAppDbContextExtensionsTestsRelated.CreateInMemoryTenantContextForTests(TENANT_ID);
 
@@ -157,8 +193,8 @@ public class GetEntriesByPeriodQueryTests
             Id = 1,
             EmployeeId = EMPLOYEE_ID,
             TenantId = TENANT_ID,
-            StartTime = new DateTime(2026, 7, 13, 0, 0, 0),
-            EndTime = new DateTime(2026, 7, 20, 0, 0, 0),
+            StartTime = sickLeaveStartTime,
+            EndTime = sickLeaveEndTime,
             Type = EntryType.SickLeave
         };
 
@@ -166,66 +202,11 @@ public class GetEntriesByPeriodQueryTests
 
         var getEntriesByPeriodQuery = new GetEntriesByPeriodQuery(context, _mockClaimsProvider);
 
-        var startDate = new DateOnly(2026, 7, 10);
-        var endDate = new DateOnly(2026, 7, 13);
+        var startDate = periodStartDate;
+        var endDate = periodEndDate;
 
         var result = await getEntriesByPeriodQuery.GetByPeriodAsync<TrackedEntryBase>(startDate, endDate);
 
-        Assert.NotEmpty(result);
-        Assert.Contains(result, x => x.Id == sickLeaveEntry.Id);
-    }
-
-    [Fact]
-    public async Task GetEntriesByPeriodAsync_ShouldNotReturnEntryWhenEntryEndsBeforePeriodStart()
-    {
-        var context = TenantAppDbContextExtensionsTestsRelated.CreateInMemoryTenantContextForTests(TENANT_ID);
-
-        var sickLeaveEntry = new SickLeaveEntry
-        {
-            Id = 1,
-            EmployeeId = EMPLOYEE_ID,
-            TenantId = TENANT_ID,
-            StartTime = new DateTime(2026, 7, 10, 0, 0, 0),
-            EndTime = new DateTime(2026, 7, 12, 0, 0, 0),
-            Type = EntryType.SickLeave
-        };
-
-        await context.AddEntityAndSaveAsync(sickLeaveEntry);
-
-        var getEntriesByPeriodQuery = new GetEntriesByPeriodQuery(context, _mockClaimsProvider);
-
-        var startDate = new DateOnly(2026, 7, 13);
-        var endDate = new DateOnly(2026, 7, 19);
-
-        var result = await getEntriesByPeriodQuery.GetByPeriodAsync<TrackedEntryBase>(startDate, endDate);
-
-        Assert.Empty(result);
-    }
-
-    [Fact]
-    public async Task GetEntriesByPeriodAsync_ShouldNotReturnEntryWhenEntryStartsAfterPeriodEnd()
-    {
-        var context = TenantAppDbContextExtensionsTestsRelated.CreateInMemoryTenantContextForTests(TENANT_ID);
-
-        var sickLeaveEntry = new SickLeaveEntry
-        {
-            Id = 1,
-            EmployeeId = EMPLOYEE_ID,
-            TenantId = TENANT_ID,
-            StartTime = new DateTime(2026, 7, 20, 0, 0, 0),
-            EndTime = new DateTime(2026, 7, 26, 0, 0, 0),
-            Type = EntryType.SickLeave
-        };
-
-        await context.AddEntityAndSaveAsync(sickLeaveEntry);
-
-        var getEntriesByPeriodQuery = new GetEntriesByPeriodQuery(context, _mockClaimsProvider);
-
-        var startDate = new DateOnly(2026, 7, 13);
-        var endDate = new DateOnly(2026, 7, 19);
-
-        var result = await getEntriesByPeriodQuery.GetByPeriodAsync<TrackedEntryBase>(startDate, endDate);
-
-        Assert.Empty(result);
+        Assert.Equal(shouldEntryBeReturned, result.Any(x => x.Id == sickLeaveEntry.Id));
     }
 }
