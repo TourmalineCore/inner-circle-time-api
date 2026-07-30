fe17259d60087a9b6fb8df888bb6e57e98c8c54f
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-07-30 11:12:00 +0500

    refactor: #118: add JsonIgnore attribute for id to all update requests to hide this field in swagger because id filled from the URL segment (query parameter) and passed here manually later in the controller and it does not make sense to initially pass it to the request

--- Application/Features/Tracking/UpdateTaskEntry/UpdateTaskEntryRequest.cs
+++ Application/Features/Tracking/UpdateTaskEntry/UpdateTaskEntryRequest.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Application.Features.Tracking.UpdateTaskEntry;
 
 public class UpdateTaskEntryRequest
 {
+    [JsonIgnore]
     public long Id { get; set; }
 
     [Required]
