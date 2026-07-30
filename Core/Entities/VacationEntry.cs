c4daf9101b8fb071656414fdf1210b80505b14fe
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-07-30 13:41:17 +0500

    fix: #110: fix type in VacationEntry constructor

--- Core/Entities/VacationEntry.cs
+++ Core/Entities/VacationEntry.cs
@@ -7,7 +7,7 @@ public class VacationEntry : TrackedEntryBase
     public VacationEntry()
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     {
-        Type = EntryType.SickLeave;
+        Type = EntryType.Vacation;
     }
 
     public bool IsUnpaid { get; set; }
