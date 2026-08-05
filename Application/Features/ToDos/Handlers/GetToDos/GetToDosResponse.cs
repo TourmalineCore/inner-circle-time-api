a75b880be8c9004007785e2a31ee11ebe758c22d
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2026-08-05 17:49:57 +0000

    chore: add DeleteToDoResponse

--- Application/Features/ToDos/Handlers/GetToDos/GetToDosResponse.cs
+++ Application/Features/ToDos/Handlers/GetToDos/GetToDosResponse.cs
@@ -14,5 +14,4 @@ public class ToDoDto
     public required string Name { get; set; }
 
     public required ToDoStatus Status { get; set; }
-
 }
---
+++ Application/Features/ToDos/Handlers/HardDeleteToDo/DeleteToDoResponse.cs
@@ -0,0 +1,6 @@
+namespace Application.Features.ToDos.Handlers.HardDeleteToDo;
+
+public class DeleteToDoResponse
+{
+    public required bool IsDeleted { get; set; }
+}
