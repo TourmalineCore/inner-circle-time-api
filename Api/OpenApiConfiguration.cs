6ba9b2a689d7f2dc5f3a2e6baa9db9e7549dda7c
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-08-24 15:04:51 +0500

    fix: return configuration for custom method name without controller name

--- Api/OpenApiConfiguration.cs
+++ Api/OpenApiConfiguration.cs
@@ -19,6 +19,18 @@ public static class OpenApiConfiguration
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         services.AddOpenApi(options =>
         {
+            options.AddOperationTransformer((operation, context, _) =>
+            {
+                // Try to get the ControllerActionDescriptor to access method information
+                if (context.Description.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+                {
+                    // Set the operationId to the ControllerName and ActionName (which is typically the method name)
+                    // This allows to have unique operationId even if there is the same method name across multiple controllers
+                    operation.OperationId = $"{controllerActionDescriptor.ActionName}";
+                }
+
+                return Task.CompletedTask;
+            });
 
             options.AddDocumentTransformer((document, context, cancellationToken) =>
             {
