bb0bacd55f9525ca3494819efa9faa01aa419d61
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-08-25 16:26:13 +0500

    infra: change in-memory db to in-memory SQLite db to make our tests run in a more realistic environment and also so that we can check more functionality in Unit tests, for example, the code where ExecuteUpdateAsync is used (#133)
    * refactor: change in InMemoryDatabase to SQLite db to make our tests run in a more realistic environment
    
    * docs(readme): bring test coverage score up to date
    
    * Apply suggestion from @fpandyz
    
    ---------
    
    Co-authored-by: Workflow Action <contact@tourmalinecore.com>
    Co-authored-by: fpandyz <5389368+fpandyz@users.noreply.github.com>

--- Application/TenantAppDbContextExtensionsTestsRelated.cs
+++ Application/TenantAppDbContextExtensionsTestsRelated.cs
@@ -1,4 +1,5 @@
 ﻿using Core;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace Application;
@@ -7,23 +8,24 @@ internal static class TenantAppDbContextExtensionsTestsRelated
 {
     public static TenantAppDbContext CreateInMemoryTenantContextForTests(long tenantId)
     {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open();
+
         var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseInMemoryDatabase(
-                // we need a unique db name so that tests of the same collection can run in isolation
-                // otherwise they inrefere and see each others data
-                new Random().Next().ToString(),
-                // we want to provide as little setup data as possible to check a certain piece of a flow
-                // thus, we don't want to specify all properties of seeded data when it isn't used by the logic
-                // for instance, I need to check that an entity exists by Id, I don't need to setup its required Name property
-                // this option allows me to bypass requited non-nullable Name check
-                x => x.EnableNullChecks(false)
-            )
+            .UseSqlite(connection)
             .Options;
 
-        return new TenantAppDbContext(
-            options,
-            tenantId
-        );
+        var context = new TenantAppDbContext(options, tenantId);
+
+        // creates a database and tables based on our model
+        context.Database.EnsureCreated();
+
+        // we need to turn off PostgreSQL related check constraints in this mode since SQLite doesn't support them
+        context.Database.ExecuteSqlRaw(@"
+            PRAGMA ignore_check_constraints = ON;
+        ");
+
+        return context;
     }
 
     public async static Task<TEntity> AddEntityAndSaveAsync<TEntity>(
